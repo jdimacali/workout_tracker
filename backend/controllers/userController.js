e4ff1dd7a74c97678c343806796e486a7c8ae01c
James Dimacali <frostbitezebra421@gmail.com>
2023-09-28 17:28:21 -0700

    signup api route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,3 @@
-const mongoose = require("mongoose");
 const User = require("../models/User");
 
 // login user
@@ -9,9 +8,16 @@ const loginUser = async (req, res) => {
 
 // signup user
 const singupUser = async (req, res) => {
+  // grab the email and password from request body
   const { email, password } = req.body;
-  const user = User.create({ email, user });
-  res.json({ message: "signup User" });
+
+  try {
+    // Use the signup static method created in userSchema
+    const user = await User.signup(email, password);
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 module.exports = { loginUser, singupUser };
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
@@ -14,5 +16,38 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// static signup method
+userSchema.statics.signup = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  // check if the email is valid
+  if (!validator.isEmail(email)) {
+    throw Error("Email is invalid");
+  }
+
+  // check if the password is valid
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password is not strong");
+  }
+  // check if email already exists
+  const existingEmail = await this.findOne({ email });
+
+  // if the email already exists then throw an error
+  if (existingEmail) {
+    throw Error("Email already in use");
+  }
+
+  // if the email does not exist then hash the password using bcrypt
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  // create new user with email and hashed password
+  const user = this.create({ email: email, password: hash });
+
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
